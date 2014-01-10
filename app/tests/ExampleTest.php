d24a8481bef1b9e04a8180dbfcc1de257498d6aa
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-01-10 12:28:21 +0000

    Updated framework from 4.0 to 4.1

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -7,23 +7,11 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testHome()
+	public function testBasicExample()
 	{
 		$crawler = $this->client->request('GET', '/');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
-    
-    /**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testSubjectsIndex()
-	{
-		$crawler = $this->action('GET', 'SubjectController@index');
-
-        $this->assertTrue($this->client->getResponse()->isOk());
-    }
 
 }
